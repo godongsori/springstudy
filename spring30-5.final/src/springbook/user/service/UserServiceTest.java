c71f371d662ac79322e6fd00b76125bc9bda6899
godongsori <godongsori@gmail.com>
2013-11-11 11:33:21 +0900

    6.3.3 다이내믹 프록시를 이용한 트랜잭션 부가기능

--- spring30-5.final/src/springbook/user/service/UserServiceTest.java
+++ spring30-5.final/src/springbook/user/service/UserServiceTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.fail;
 import static springbook.user.service.UserServiceImpl.MIN_LOGCOUNT_FOR_SILVER;
 import static springbook.user.service.UserServiceImpl.MIN_RECCOMEND_FOR_GOLD;
 
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -190,9 +191,15 @@ public class UserServiceTest {
 		testUserService.setUserDao(this.userDao);
 		testUserService.setMailSender(this.mailSender);
 		
-		UserServiceTx txUserService = new UserServiceTx();
-		txUserService.setTransactionManager(this.transactionManager);
-		txUserService.setUserService(testUserService);
+		TransactionHandler txHandler = new TransactionHandler();
+		txHandler.setTarget(testUserService);
+		txHandler.setTransactionManager(transactionManager);
+		txHandler.setPattern("upgradeLevels");
+
+		UserService txUserService = (UserService)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[]{UserService.class}, 
+				txHandler);
 		
 		userDao.deleteAll();			  
 		for(User user : users) userDao.add(user);
